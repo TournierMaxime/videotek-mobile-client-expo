bb0d0a3167e9569afe31db91c56c079ac706c738
Maxime Tournier <mtournier@devsdefrance.com>
2023-07-04 23:03:11 +0200

    hot fix

--- src/components/Languages.js
+++ src/components/Languages.js
@@ -5,7 +5,6 @@ import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export default function Languages({ i18n }) {
   const [lang, setLang] = useState(i18n.language)
-  console.log(lang)
 
   const itemsPicker = [
     { label: 'German', value: 'de-DE' },
