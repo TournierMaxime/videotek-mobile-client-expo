d4195324d66eaed31fc54eafca24ae33cf7f28b9
Maxime Tournier <mtournier@devsdefrance.com>
2024-03-21 15:28:01 +0100

    dark mode

--- App.js
+++ App.js
@@ -1,25 +1,25 @@
-import { connect, Provider } from 'react-redux'
-import React, { Fragment, useEffect } from 'react'
-import { NavigationContainer } from '@react-navigation/native'
-import store from './src/redux/store'
-import Header from '@mod/mobile-common/lib/components/layout/Header'
-import Trending from '@mod/mobile-tmdb/views/Movies/Trending'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { MaterialIcons, Ionicons, Entypo } from 'react-native-vector-icons'
-import { useTranslation } from 'react-i18next'
-import i18n from './i18n'
-import { I18nextProvider } from 'react-i18next'
-import './polyfill'
-import { Platform, Dimensions } from 'react-native'
-import Utils from '@mod/mobile-common/lib/class/Utils'
-import AuthStackNavigator from '@mod/mobile-auth/navigators/AuthStackNavigator'
-import MainStackNavigator from './src/navigators/MainStackNavigator.js'
-import useLocalStorage from '@mod/mobile-common/lib/hooks/utils/useLocalStorage'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import Movies from './src/views/Movies.js'
-import Series from './src/views/Series.js'
-import { useDynamicThemeStyles } from '@mod/mobile-common/styles/theme'
-import { useSelector } from 'react-redux'
+import { connect, Provider } from "react-redux"
+import React, { Fragment, useEffect } from "react"
+import { NavigationContainer } from "@react-navigation/native"
+import store from "./src/redux/store"
+import Header from "@mod/mobile-common/lib/components/layout/Header"
+import Trending from "@mod/mobile-tmdb/views/Movies/Trending"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import { MaterialIcons, Ionicons, Entypo } from "react-native-vector-icons"
+import { useTranslation } from "react-i18next"
+import i18n from "./i18n"
+import { I18nextProvider } from "react-i18next"
+import "./polyfill"
+import { Platform, Dimensions } from "react-native"
+import Utils from "@mod/mobile-common/lib/class/Utils"
+import AuthStackNavigator from "@mod/mobile-auth/navigators/AuthStackNavigator"
+import MainStackNavigator from "./src/navigators/MainStackNavigator.js"
+import useLocalStorage from "@mod/mobile-common/lib/hooks/utils/useLocalStorage"
+import { QueryClient, QueryClientProvider } from "react-query"
+import Movies from "./src/views/Movies.js"
+import Series from "./src/views/Series.js"
+import { useDynamicThemeStyles } from "@mod/mobile-common/styles/theme"
+import { useSelector } from "react-redux"
 
 const queryClient = new QueryClient()
 
@@ -34,7 +34,8 @@ const App = ({ isAuthenticated, onLoginSuccess }) => {
 
   const darkMode = useSelector((state) => state.theme.darkMode)
 
-  const { colorIcon, backgroundTabColor } = useDynamicThemeStyles(darkMode)
+  const { colorIcon, backgroundTabColor, activeIcon } =
+    useDynamicThemeStyles(darkMode)
 
   useEffect(() => {
     getUserData()
@@ -51,36 +52,36 @@ const App = ({ isAuthenticated, onLoginSuccess }) => {
             marginTop: Utils.moderateScale(10),
           },
           tabBarStyle: {
-            ...(Platform.OS === 'android'
+            ...(Platform.OS === "android"
               ? { marginLeft: Utils.moderateScale(0) }
               : { marginLeft: Utils.moderateScale(15) }),
-            marginRight: 'auto',
-            ...(Dimensions.get('window').width > 600
+            marginRight: "auto",
+            ...(Dimensions.get("window").width > 600
               ? { height: Utils.moderateScale(50) }
               : null),
-            justifyContent: 'center',
-            display: 'flex',
-            flexDirection: 'row',
+            justifyContent: "center",
+            display: "flex",
+            flexDirection: "row",
             zIndex: 0,
-            backgroundColor: backgroundTabColor
+            backgroundColor: backgroundTabColor,
           },
         }}
       >
         {isAuthenticated === true ? (
           <Fragment>
             <Tab.Screen
-              name='MainStackNavigator'
+              name="MainStackNavigator"
               options={() => ({
-                tabBarIcon: () => (
+                tabBarIcon: ({ focused }) => (
                   <Entypo
-                    style={{ width: Utils.moderateScale(50), height: 'auto' }}
-                    name='home'
+                    style={{ width: Utils.moderateScale(50), height: "auto" }}
+                    name="home"
                     size={Utils.moderateScale(25)}
-                    color={colorIcon}
+                    color={focused ? activeIcon : colorIcon}
                   />
                 ),
                 headerShown: false,
-                tabBarLabel: '',
+                tabBarLabel: "",
               })}
             >
               {() => (
@@ -93,78 +94,80 @@ const App = ({ isAuthenticated, onLoginSuccess }) => {
             </Tab.Screen>
 
             <Tab.Screen
-              name='Trending'
+              name="Trending"
               component={Trending}
               options={() => ({
-                tabBarIcon: () => (
+                tabBarIcon: ({ focused }) => (
                   <Ionicons
-                    style={{ width: Utils.moderateScale(50), height: 'auto' }}
-                    name='flame'
+                    style={{ width: Utils.moderateScale(50), height: "auto" }}
+                    name="flame"
                     size={Utils.moderateScale(25)}
-                    color={colorIcon}
+                    color={focused ? activeIcon : colorIcon}
                   />
                 ),
                 header: () => (
                   <Header isAuthenticated={isAuthenticated} backButton={true} />
                 ),
                 headerShown: true,
-                tabBarLabel: '',
+                tabBarLabel: "",
+                tabBarActiveTintColor: activeIcon,
               })}
             />
             <Tab.Screen
-              name='Movies'
+              name="Movies"
               component={Movies}
               options={() => ({
-                tabBarIcon: () => (
+                tabBarIcon: ({ focused }) => (
                   <MaterialIcons
-                    style={{ width: Utils.moderateScale(50), height: 'auto' }}
-                    name='movie'
+                    style={{ width: Utils.moderateScale(50), height: "auto" }}
+                    name="movie"
                     size={Utils.moderateScale(25)}
-                    color={colorIcon}
+                    color={focused ? activeIcon : colorIcon}
                   />
                 ),
                 header: () => (
                   <Header isAuthenticated={isAuthenticated} backButton={true} />
                 ),
                 headerShown: true,
-                tabBarLabel: '',
+                tabBarLabel: "",
+                tabBarActiveTintColor: activeIcon,
               })}
             />
 
             <Tab.Screen
-              name='Series'
+              name="Series"
               component={Series}
               options={() => ({
-                tabBarIcon: () => (
+                tabBarIcon: ({ focused }) => (
                   <Ionicons
-                    style={{ width: Utils.moderateScale(50), height: 'auto' }}
-                    name='tv-sharp'
+                    style={{ width: Utils.moderateScale(50), height: "auto" }}
+                    name="tv-sharp"
                     size={Utils.moderateScale(25)}
-                    color={colorIcon}
+                    color={focused ? activeIcon : colorIcon}
                   />
                 ),
                 header: () => (
                   <Header isAuthenticated={isAuthenticated} backButton={true} />
                 ),
                 headerShown: true,
-                tabBarLabel: '',
+                tabBarLabel: "",
               })}
             />
           </Fragment>
         ) : (
           <Tab.Screen
-            name='AuthStackNavigator'
+            name="AuthStackNavigator"
             options={() => ({
-              tabBarIcon: () => (
+              tabBarIcon: ({ focused }) => (
                 <Entypo
-                  style={{ width: Utils.moderateScale(50), height: 'auto' }}
-                  name='home'
+                  style={{ width: Utils.moderateScale(50), height: "auto" }}
+                  name="home"
                   size={Utils.moderateScale(25)}
-                  color={colorIcon}
+                  color={focused ? activeIcon : colorIcon}
                 />
               ),
               headerShown: false,
-              tabBarLabel: '',
+              tabBarLabel: "",
             })}
           >
             {() => (
@@ -188,7 +191,7 @@ const mapStateToProps = (state) => ({
 
 const login = (dispatch) => ({
   onLoginSuccess: (userData) =>
-    dispatch({ type: 'LOGIN_USER_SUCCESS', payload: userData }),
+    dispatch({ type: "LOGIN_USER_SUCCESS", payload: userData }),
 })
 
 const ConnectedApp = connect(mapStateToProps, login)(App)
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,29 +1,29 @@
-import React, { useEffect, Fragment } from 'react'
-import { ActivityIndicator, ScrollView } from 'react-native'
-import { nowPlaying, trending } from '@mod/mobile-tmdb/react-query/movies'
-import { trendingTV } from '@mod/mobile-tmdb/react-query/series'
-import { useTranslation } from 'react-i18next'
-import registerForPushNotificationsAsync from '@mod/mobile-common/lib/components/utils/Notifications'
-import Trending from './Movies/Trending'
-import NowPlaying from './Movies/NowPlaying'
-import TrendingTV from './Series/TrendingTV'
-import { useQuery } from 'react-query'
-import useNotification from '@mod/mobile-common/lib/hooks/utils/useNotification.js'
+import React, { useEffect, Fragment } from "react"
+import { ActivityIndicator, ScrollView, Image } from "react-native"
+import { nowPlaying, trending } from "@mod/mobile-tmdb/react-query/movies"
+import { trendingTV } from "@mod/mobile-tmdb/react-query/series"
+import { useTranslation } from "react-i18next"
+import registerForPushNotificationsAsync from "@mod/mobile-common/lib/components/utils/Notifications"
+import Trending from "./Movies/Trending"
+import NowPlaying from "./Movies/NowPlaying"
+import TrendingTV from "./Series/TrendingTV"
+import { useQuery } from "react-query"
+import useNotification from "@mod/mobile-common/lib/hooks/utils/useNotification.js"
+import tw from "twrnc"
 
 const Home = () => {
-
   const { i18n, t } = useTranslation()
   const language = i18n.language
 
   const { data: nowPlayingData, isLoading } = useQuery(
-    ['nowPlaying', 1, language],
-    () => nowPlaying(1, language)
+    ["nowPlaying", 1, language],
+    () => nowPlaying(1, language),
   )
-  const { data: trendingData } = useQuery(['trending', 1, language], () =>
-    trending(1, language)
+  const { data: trendingData } = useQuery(["trending", 1, language], () =>
+    trending(1, language),
   )
-  const { data: trendingTVData } = useQuery(['trendingTV', 1, language], () =>
-    trendingTV(1, language)
+  const { data: trendingTVData } = useQuery(["trendingTV", 1, language], () =>
+    trendingTV(1, language),
   )
 
   useNotification()
@@ -35,7 +35,7 @@ const Home = () => {
   return (
     <Fragment>
       {isLoading ? (
-        <ActivityIndicator size='large' color='#0000ff' />
+        <ActivityIndicator size="large" color="#0000ff" />
       ) : (
         <ScrollView>
           <Trending arrow={false} trending={trendingData} t={t} />
