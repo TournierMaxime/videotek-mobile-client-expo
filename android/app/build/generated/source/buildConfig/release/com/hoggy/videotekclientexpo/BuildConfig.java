8cbe9dd24ed73e08c1edc709946b18b7d4dd8959
Maxime Tournier <mtournier@devsdefrance.com>
2024-05-28 17:06:23 +0200

    release 1.5.0

---
+++ android/app/build/generated/source/buildConfig/release/com/hoggy/videotekclientexpo/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.hoggy.videotekclientexpo;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.hoggy.videotekclientexpo";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 30;
+  public static final String VERSION_NAME = "1.5.0";
+  // Field from default config.
+  public static final boolean IS_HERMES_ENABLED = true;
+  // Field from default config.
+  public static final boolean IS_NEW_ARCHITECTURE_ENABLED = false;
+}
