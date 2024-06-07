70d020450ba7c7dc4d841acbfc91dd411bca1380
Maxime Tournier <mtournier@devsdefrance.com>
2024-06-07 14:13:47 +0200

    fix release

--- android/app/build/generated/source/buildConfig/release/com/hoggy/videotekclientexpo/BuildConfig.java
+++ android/app/build/generated/source/buildConfig/release/com/hoggy/videotekclientexpo/BuildConfig.java
@@ -7,7 +7,7 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.hoggy.videotekclientexpo";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 33;
+  public static final int VERSION_CODE = 34;
   public static final String VERSION_NAME = "1.5.2";
   // Field from default config.
   public static final boolean IS_HERMES_ENABLED = true;
