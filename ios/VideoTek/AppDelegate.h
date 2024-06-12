302e38cb9838232e65e00f02df3cef475a3069d0
Maxime Tournier <mtournier@devsdefrance.com>
2024-06-12 18:29:57 +0200

    refactoring

---
+++ ios/VideoTek/AppDelegate.h
@@ -0,0 +1,9 @@
+#import <RCTAppDelegate.h>
+#import <UIKit/UIKit.h>
+#import <Expo/Expo.h>
+#import <React/RCTLinkingManager.h>
+#import "RNAppAuthAuthorizationFlowManager.h"
+
+@interface AppDelegate : EXAppDelegateWrapper <RNAppAuthAuthorizationFlowManager>
+@property(nonatomic, weak) id<RNAppAuthAuthorizationFlowManagerDelegate> authorizationFlowManagerDelegate;
+@end
